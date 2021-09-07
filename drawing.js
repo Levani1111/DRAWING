fb04bd8d234f602a706a4e7a70b0721cc3b8f66d
Your Name <levani.papashvili@labshare.org>
2021-09-07 17:59:26 -0400

    mobile

--- drawing.js
+++ drawing.js
@@ -90,11 +90,33 @@ function handleUp()
     console.log(drawing);
 }
 
-function mouseXY (e) {
-    var touches = e.touches || [];
-    var touch = touches[0] || {};
-    if (paint) {
-        addClick(touch.pageX - this.offsetLeft, touch.pageY - this.offsetTop - 40, true);
-        draw();
+function onTouch(evt) {
+    evt.preventDefault();
+    if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
+      return;
+  
+    var newEvt = document.createEvent("MouseEvents");
+    var type = null;
+    var touch = null;
+  
+    switch (evt.type) {
+      case "touchstart":
+        type = "mousedown";
+        touch = evt.changedTouches[0];
+        break;
+      case "touchmove":
+        type = "mousemove";
+        touch = evt.changedTouches[0];
+        break;
+      case "touchend":
+        type = "mouseup";
+        touch = evt.changedTouches[0];
+        break;
     }
-}
+  
+    newEvt.initMouseEvent(type, true, true, evt.originalTarget.ownerDocument.defaultView, 0,
+      touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+      evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
+    evt.originalTarget.dispatchEvent(newEvt);
+  }
+  
